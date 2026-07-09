bc7778d176bf4704fd973304c767dfea545473db
Arham Shah <86466418+Arhamshah18@users.noreply.github.com>
2026-07-09 10:22:22 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,18 @@
+#required & keyword argument 
+def req(b,a):
+    print(f"name={a}\nRoll no.={b}")
+#default argument 
+def de(clas=2):
+    print(f"class=SY{clas}") 
+#variable args
+def va(*s):
+    print(f"Subjects={s}")
+#################
+n=input("Enter name : ")
+r=int(input("Enter Roll no. : ")) 
+wl=input("Enter subjects and seperate by commas : ")
+sub=wl.split(",")  
+clas=int(input("Enter classs :"))
+req(a=n,b=r)
+de(clas)
+va(sub) 
